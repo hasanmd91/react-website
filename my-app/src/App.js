79cf354b6f048ac24d4ed84bc4a7cc1610e622e6
hasanmd91 <hasanmd1991@hotmail.com>
2022-07-06 01:51:26 +0300

    navbar ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,37 @@
 import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 import "./App.css";
-import Home from "./Components/Home";
+import Home from "./Components/Home/Home";
 import About from "./Components/About";
 import Blogs from "./Components/Blogs";
 import Contact from "./Components/Contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import particles from "./Utils/Particles.json";
 
 const App = () => {
+  const location = useLocation();
+
+  const handelinit = async (main) => {
+    await loadFull(main);
+  };
+
+  const renderParticalsInHome = location.pathname === "/";
+
   return (
-    <div className="container">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+    <div>
+      {renderParticalsInHome && (
+        <Particles id="particles" options={particles} init={handelinit} />
+      )}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
---
+++ my-app/src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <section id="home" className="home">
+      <div className="home_text_wrppper">
+        <h1>
+          {" "}
+          Hello, I am Hasan <br /> MERN Stack developer{" "}
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
